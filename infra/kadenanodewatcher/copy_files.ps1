32da901141d1e3e4bf7bd61d0878b7a64c22f967
kubagdynia <kubagdynia@gmail.com>
2024-02-09 10:02:14 +0100

    Added docker compose and an infra layer

---
+++ infra/kadenanodewatcher/copy_files.ps1
@@ -0,0 +1,45 @@
+#
+$WebApiPath = "./webapi"
+
+$foldersToDelete = @(
+    "$WebApiPath/DbConnectionExtensions"
+    "$WebApiPath/KadenaNodeWatcher.Api"
+    "$WebApiPath/KadenaNodeWatcher.Core"
+)
+
+#### DELETE FILES
+
+foreach($folder in $foldersToDelete)
+{
+    # Check if folder exists
+    if (Test-Path $folder)
+    {
+        # Folder exists, delete it!
+        Remove-Item -Path $folder -Recurse
+        Write-host "Folder Deleted at" $dir -f Green
+    }
+    else {
+        Write-host "Folder" $folder "does not exist!" -f Red
+    }
+}
+
+#### COPY FILES
+
+$foldersToCopy = @(
+    [pscustomobject]@{Source="../../DbConnectionExtensions";Target="$WebApiPath/DbConnectionExtensions"}
+    [pscustomobject]@{Source="../../KadenaNodeWatcher.Api";Target="$WebApiPath/KadenaNodeWatcher.Api"}
+    [pscustomobject]@{Source="../../KadenaNodeWatcher.Core";Target="$WebApiPath/KadenaNodeWatcher.Core"}
+)
+
+foreach($data in $foldersToCopy)
+{
+    # Check if folder exists
+    if (Test-Path $data.Source)
+    {
+        Copy-Item $data.Source -Destination $data.Target -Recurse
+        Write-Host "Folder Copied from" $data.Source "To" $data.Target -f Green
+    }
+    else {
+        Write-host "Folder" $data.Source "does not exist!" -f Red
+    }
+}
